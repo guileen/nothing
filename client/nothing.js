277c3a96076332068d20fa21d980a6a3a60f5ccf
Lin Gui <guileen@gmail.com>
2012-03-13 00:47:29 +0800

    add parseUrl

--- client/nothing.js
+++ client/nothing.js
@@ -85,6 +85,19 @@ String.prototype.format = function() {
   return formatted;
 };
 
+// this function is use to strip url not validation
+function parseUrl (url) {
+  var r = /^(\w+):\/\/([^\/\?#]+)(\/[^\?#]*)?(\?[^#]*)?(#.*)?$/i.exec(url);
+  return r && {
+    schema : r[1]
+  , domain : r[2]
+  , root : r[1] + '://' + r[2]
+  , path : r[3]
+  , querystring : r[4]
+  , anchor : r[5]
+  };
+}
+
 // QueryString
 function parseQueryString(queryString) {
   var result = {};
